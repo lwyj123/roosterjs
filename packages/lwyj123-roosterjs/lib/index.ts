fdb020b4cbd48f1e0c6a5894e8a5a7d332f567af
Yuyi Liang <yuyiliang@microsoft.com>
2023-06-22 17:23:23 +0800

    feat: delete root package

--- packages/lwyj123-roosterjs/lib/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as createEditor } from './createEditor';
-export * from 'lwyj123-roosterjs-editor-types';
-export * from 'lwyj123-roosterjs-editor-types-compatible';
-export * from 'lwyj123-roosterjs-editor-dom';
-export * from 'lwyj123-roosterjs-editor-core';
-export * from 'lwyj123-roosterjs-editor-api';
-export * from 'lwyj123-roosterjs-editor-plugins';
-export * from 'lwyj123-roosterjs-color-utils';
